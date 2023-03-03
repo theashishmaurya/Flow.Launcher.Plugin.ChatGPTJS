652013dcf35bd82922592489c5066b89e53f1822
AshishIvoyant <ashish.1999vns@gmail.com>
2023-03-03 15:51:05 +0530

    added open ai

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ if (method === "query") {
             "result": [{
                 "Title": "Hello World Typescript",
                 "Subtitle": "Showing your query parameters: " + parameters + ". Click to open Flow's website",
+                // Here we use the method name "do_something_for_query" to call the method "do_something_for_query" in the plugin
                 "JsonRPCAction": {
                     "method": "do_something_for_query",
                     "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"]

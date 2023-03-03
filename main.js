4578f60783e74947404e6eae40a28d57b9d06aaa
AshishIvoyant <ashish.1999vns@gmail.com>
2023-03-03 15:38:20 +0530

    Added Plugin JSON

---
+++ main.js
@@ -0,0 +1,30 @@
+const open = require('open');
+
+const { method, parameters } = JSON.parse(process.argv[2])
+
+
+if (method === "query") {
+    console.log(JSON.stringify(
+        {
+            "result": [{
+                "Title": "Hello World Typescript",
+                "Subtitle": "Showing your query parameters: " + parameters + ". Click to open Flow's website",
+                "JsonRPCAction": {
+                    "method": "do_something_for_query",
+                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"]
+                },
+                "IcoPath": "Images\\app.png",
+                "score" : 0
+            }]
+        }
+    ));
+}
+
+if (method === "do_something_for_query") {
+    url = parameters[0]
+    do_something_for_query(url)
+}
+
+function do_something_for_query(url) {
+    open(url);
+}
